106780963b159c4cc20a4923b7834deb0ae89a99
Robert Krug <robert.krug@oru.se>
2017-09-21 16:51:22 +0200

    added some python stuff

--- init.el
+++ init.el
@@ -112,6 +112,27 @@
 ;;(add-hook 'LaTeX-mode-hook 'LaTeX-math-mode)
 
 ;;============== PYTHON ===================
+;; enable elpy mode
+(elpy-enable)
 
+;;need to set this variable do avoid argument errors in ipython
+(setenv "IPY_TEST_SIMPLE_PROMPT" "1")
 
+;;use ipython by default
+(when (executable-find "ipython")
+  (setq python-shell-interpreter "ipython"))
 
+(custom-set-variables
+ ;; custom-set-variables was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ '(package-selected-packages
+   (quote
+    (ein irony-eldoc flycheck-irony elpy company-irony company-c-headers clang-format))))
+(custom-set-faces
+ ;; custom-set-faces was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ )
